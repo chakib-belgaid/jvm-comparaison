4de3cb16ec30ac814c7edabee1cd5629ca67b11e
Mohammed Chakib Belgaid <mbelgaid@frontend-buster>
2020-11-20 14:02:45 +0100

    update : benchmakrs

--- benchmarks/benchmarks.sh
+++ benchmarks/benchmarks.sh
@@ -1,3 +1,5 @@
-dacapo.jar avrora -s large 
+# dacapo.jar avrora -s large 
 # renaissance-gpl-0.11.0.jar als -r 10
 # renaissance-gpl-0.11.0.jar dotty -r 10
+renaissance-gpl-0.11.0.jar scrabble -r 20 
+renaissance-gpl-0.11.0.jar neo4j-analytics -r 7 
