1ecb59257a5132cec1dc50e338fbfb87ccb2624e
Mohammed Chakib Belgaid <mbelgaid@frontend-buster>
2020-11-25 12:10:19 +0100

    fix : typo

--- record.sh
+++ record.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+#to use this you have to launch the all-records.sh script
+# this is ment to be used inside the docker continer
+# If you want to use it alone you can call it directly and pass the reporting file name before the command
+##  example : ./record.sh myreport.log [[CMD]]
+
 reportfile=/exp/records/$1.top
 shift
 nohup $@ &
