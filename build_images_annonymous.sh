a24c8ed43b084811816b0b48b91c87869fed6d31
chakib-belgaid <chakib.belgaid@gmail.com>
2021-02-23 23:14:27 +0100

    update : remove username from scripts

--- build_images_annonymous.sh
+++ build_images_annonymous.sh
@@ -2,16 +2,14 @@
 IFS=$'\n'
 jvms=$(grep -v "#" jvms.sh)
 
-user=chakibmed
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+user=user
 
 for jvm in ${jvms[@]}; do
     echo building jvm $jvm
-    sdk install java $jvm
+    docker build --tag $user/jvm:$jvm --build-arg TAG="$jvm" -f jvms-builder/Dockerfile jvms-builder
     # docker push $user/jvm:$jvm
 done
 
-mkdir benchmarks/jars && cd benchmarks/jars &&
-
+mkdir benchmarks/jars && cd benchmarks/jars
+# mkdir benchmarks/jars && cd benchmarks/jars &&
 # download dacapo.jar and renaissance.jar in benchmarks/jar
